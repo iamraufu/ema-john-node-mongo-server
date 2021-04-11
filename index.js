3bc9617e1e69ef37ed712f57a610b71409bbf35a
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-11 16:37:02 +0600

    final commit - dynamic search added

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ client.connect(err => {
     })
 
     app.get('/products', (req, res) => {
-        productsCollection.find({})
+        const search = req.query.search
+        productsCollection.find({ name: { $regex: search } })
             .toArray((err, documents) => {
                 res.send(documents);
             })
